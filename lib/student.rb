445b65b460a81cfe876e474a3052ac1eabc791cf
Carrie Savinell <carrielane89@gmail.com>
2018-06-28 21:19:48 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -100,7 +100,11 @@ class Student
     SQL
     DB[:conn].execute(sql).map do |row|
       self.new_from_db(row)
-    end.first   
+    end.first
+  end
+
+  def self.all_students_in_grade_X
+    
   end
 
   def self.drop_table
