5513b722c98a37f4f84008868f497aa97848b459
Carrie Savinell <carrielane89@gmail.com>
2018-06-28 19:56:00 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -16,6 +16,11 @@ class Student
 
   def self.find_by_name(name)
     # find the student in the database given a name
+    sql = <<-SQL
+      "SELECT *
+       FROM students
+       WHERE "
+    SQL
     # return a new instance of the Student class
   end
 
